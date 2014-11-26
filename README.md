e9e04b849e2569e347709968613b67542cde8a88
Arpad Kovacs <minhmeoke@gmail.com>
2014-11-26 14:55:46 -0800

    Add uncommitted code from initial implementation spike

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Scalable web cache using consistent hashing for Stanford CS 244b.
 
 Menu > Help > Install New Software > Add...
 
-Name: M2Eclipse
-Location: http://download.eclipse.org/technology/m2e/releases
+* Name: M2Eclipse
+* Location: http://download.eclipse.org/technology/m2e/releases
 
 Select All > Next > Finish
 
@@ -16,20 +16,14 @@ File > Import > Maven > Existing Maven Projects
 
 ## Run in Eclipse
 Menu > Run > Run Configurations
-Name: Server
-Project: cs244b-final-project
-Main Class: edu.stanford.cs244b.Server
-Arguments Tab > Program Arguments: server
-Run
 
-Browse to http://localhost:8080/shard/0
-You should see Shard=0 Hits=1
-The hits counter increases on each page refresh (eg: each time you send a GET to the /shard/0 endpoint).
+* Name: Server
+* Project: cs244b-final-project
+* Main Class: edu.stanford.cs244b.Server
+* Arguments Tab > Program Arguments: server
 
-Browse to http://localhost:8080/swagger#!/shard
-There are 2 endpoints defined, the first is the shard from before
-The second is the router which will dispatch requests to the correct shard.
-Note that the router is currently hardcoded to redirect requests to shard 0.
+Browse to [http://localhost:8080/shard/0](http://localhost:8080/shard/0); you should see {shard: 0, hits: 1}. The hits counter increases on each page refresh (eg: each time you send a GET to the /shard/0 endpoint).
 
-Browse to http://localhost:8081/metrics?pretty=true
-Scroll down to view latency metrics
+Browse to [http://localhost:8080/swagger](http://localhost:8080/swagger). There are 2 endpoints defined, the first is the [shard](http://localhost:8080/swagger#!/shard) from before. The second is the [router](http://localhost:8080/swagger#!/router) which will dispatch requests to the correct shard. Note that the router is currently hardcoded to redirect requests to shard 0.
+
+Browse to [http://localhost:8081/metrics?pretty=true](http://localhost:8081/metrics?pretty=true). Scroll down to view latency metrics for the shard and router.
--- src/edu/stanford/cs244b/Shard.java
+++ src/edu/stanford/cs244b/Shard.java
@@ -1,7 +1,9 @@
 package edu.stanford.cs244b;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -10,34 +12,62 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 /** Shards receive incoming requests from the router,
  *  process the request and return it to the client. 
  */
-@Path("/shard")
-@Api("/shard")
+@Path("/shard/0")
+//TODO: @Path("/{shardId}")
+@Api("/shard/0")
 // TODO: return JSON? or some binary format like protobuf?
 @Produces(MediaType.APPLICATION_JSON)
 public class Shard {
     private final int shardId;
     private final AtomicLong counter;
+    
+    private Map<String,String> storage = new HashMap<String,String>();
 
     public Shard(int shardId) {
         this.shardId = shardId;
         this.counter = new AtomicLong();
     }
-
-    @GET
-    @Timed
-    @Path("/0")
-    // TODO: @Path("/{shardId}")
-    @ApiOperation("Record a request to this shard")
-    public HashMap<String,Object> getHits() {
-    // TODO: public HashMap<String,Object> getHits(@PathParam("shardId")) {
+    
+    /** perform basic operations which apply for each request,
+     *  eg: populate hashmap containing shardId, record a hit to the shard
+     *  (maybe for load-balancing purposes) 
+     */
+    private HashMap<String,Object> processRequest() {
         return new HashMap<String,Object>() {{
             put("shard", shardId);
             put("hits", counter.incrementAndGet());
         }};
+    }
+
+    /** Insert a new item into the distributed hash table */
+    /*@POST
+    @Timed
+    @ApiOperation("Insert a new item into the distributed hash table")
+    public Map<String,Object> insertItem(Object obj) {
+        
+    }*/
+    
+    /** Update an existing item in the distributed hash table */
+    //@PUT 
+
+    @GET
+    @Timed
+    @Path("/shard/0/{itemId}")
+    @ApiOperation("Retrieve an item from this shard, or return 404 Not Found if it does not exist")
+    public Map<String,Object> getItem(@PathParam("itemId") String itemId) {
+        Map<String, Object> results = processRequest();
+        Object item = storage.get(itemId);
+        if (item == null) {
+            //throw new Exception
+        } else {
+            results.put(itemId, item);
+        }
+        return results;
     }
 }
