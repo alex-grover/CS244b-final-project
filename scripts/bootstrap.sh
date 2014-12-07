78f1315a94e4c992b9702b9a5725a0a01689c8f5
Arpad Kovacs <minhmeoke@gmail.com>
2014-12-07 12:01:14 -0800

    Use Ubuntu server 14.04 instead of ArchLinux due to Vagrant eth* networking issue (https://github.com/mitchellh/vagrant/issues/4288)

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,11 +2,7 @@
 
 # Install OpenJDK 7
 if ! type java > /dev/null; then
-    # remove package manager lock in case of premature system shutdown
-    if [ -f /var/lib/pacman/db.lck ]; then
-        sudo rm /var/lib/pacman/db.lck
-    fi
     # install java
-    pacman -Sy
-    pacman --noconfirm -S jdk7-openjdk
+    sudo apt-get update
+    sudo apt-get -y install openjdk-7-jre 
 fi
