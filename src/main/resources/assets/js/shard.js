6e380d91cf4a440a46e8d9ac9df0e086688454d9
Arpad Kovacs <minhmeoke@gmail.com>
2014-12-11 13:12:28 -0800

    Add table listing uploaded files

--- src/main/resources/assets/js/shard.js
+++ src/main/resources/assets/js/shard.js
@@ -1,10 +1,21 @@
-var app = angular.module('main', []).
-  controller('ShardCtrl', function($scope, $http) {
-    $scope.meta = {'shard': '(unknown shardid)'};
+var app = angular.module('main', ['ngTable']).
+  controller('ShardCtrl', function($scope, $http, ngTableParams) {
+    $scope.meta = {'shard': '(unknown shardid)',
+                   'files': []};
+
+    $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10           // count per page
+    }, {
+        total: $scope.meta.files.length, // length of data
+        getData: function($defer, params) {
+            $defer.resolve($scope.meta.files.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
 
     $scope.refresh = function() {
         $http.get('/api/shard/meta').success(function(data, status, headers, config) {
-            $scope.files = data;
+            $scope.meta = data;
         });
     };
 });
